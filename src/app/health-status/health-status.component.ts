d05857a23b7fdfb40917a0e15e81215ea26b0faf
akhilesh tiwari <akhilesh.tiwari10@gmail.com>
2019-05-18 18:40:09 -0400

    added expand collapse

--- src/app/health-status/health-status.component.ts
+++ src/app/health-status/health-status.component.ts
@@ -10,10 +10,30 @@ export class HealthStatusComponent implements OnInit {
 
   deviceOn:boolean = true;
   health:string = "Warnning";
+  showOptions = false;
+  showSysStatus = false;
+  showFNOL = false;
+  showQuestions = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onOptionsClick(){
+    this.showOptions = this.showOptions === true ? false : true;
+  }
+
+  onOSysStatusClick(){
+    this.showSysStatus = this.showSysStatus === true ? false : true;
+  }
+
+  onFNOLClick(){
+    this.showFNOL = this.showFNOL === true ? false : true;
+  }
+
+  onQuestionsClick(){
+    this.showQuestions = this.showQuestions === true ? false : true;
   }
 
 }
