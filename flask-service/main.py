42cb30ecd2012fe04d6ea3f09789427df38ceda7
Saurabh <Tushar.Saurabh2@chubb.com>
2019-05-18 12:28:18 -0400

    Second commit

--- flask-service/main.py
+++ flask-service/main.py
@@ -9,19 +9,34 @@ def fetch():
     records = []
     val = random.randrange(1,4)
     print (val)
+    devices = ['humidity','waterleak']
     if val <= 1:
         for i in range(100):
-            humidity = random.randrange(1,50)
-            records.append({"deviceId":"doris","temperature":random.random()*30,"humidity":humidity,"pointInfo":"This is a critical message."}) 
+            
+            deviceName = devices[random.randint(0,1)]
+            
+            if deviceName == 'humidity':
+                humidity = random.randrange(1,50)
+                records.append({"deviceId":"humidity","temperature":random.random()*30,"humidity":humidity,"pointInfo":"This is a critical message."}) 
+            elif deviceName == 'waterleak':
+                records.append({"deviceId":deviceName,"leak":"False","pointInfo":"This is a critical message."}) 
+            
+            
     elif val <= 2:
         for i in range(100):
-            humidity = random.randrange(1,80)
-            records.append({"deviceId":"doris","temperature":random.random()*30,"humidity":humidity,"pointInfo":"This is a critical message."}) 
+            deviceName = devices[random.randint(0,1)]
+            if deviceName == 'humidity':
+                humidity = random.randrange(1,80)
+                records.append({"deviceId":"humidity","temperature":random.random()*30,"humidity":humidity,"pointInfo":"This is a critical message."}) 
+            elif deviceName == 'waterleak':
+                records.append({"deviceId":deviceName,"leak":"False","pointInfo":"This is a critical message."}) 
     else:
         for i in range(100):
-            records.append({"deviceId":"doris","temperature":random.random()*30,"humidity":random.random()*100,"pointInfo":"This is a critical message."}) 
-    
-    
+            deviceName = devices[random.randint(0,1)]
+            if deviceName == 'humidity':
+                records.append({"deviceId":"humidity","temperature":random.random()*30,"humidity":random.random()*100,"pointInfo":"This is a critical message."}) 
+            elif deviceName == 'waterleak':
+                records.append({"deviceId":deviceName,"leak":str(bool(random.randint(0,1))),"pointInfo":"This is a critical message."}) 
         
 
     resp = Response(json.dumps(records))
